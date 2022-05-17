da891a890a0eb922ee56d26287107d8a3d291157
Paul Chesa <paulchesa1@gmail.com>
2022-05-17 06:45:49 +0300

    ✨Add button to edit single bulb

--- src/components/lights.component.tsx
+++ src/components/lights.component.tsx
@@ -1,18 +1,32 @@
-import { Avatar } from "antd";
-import React, { useEffect} from "react";
+import { Avatar, Button, Modal } from "antd";
+import React, { useEffect, useState} from "react";
 import { IAllLights } from "../model/all-lights.model";
 import { ILight } from "../model/light.model";
 import { LightsService } from "../services/lights.service";
 import { useAppDispatch, useAppSelector } from "../state/hook";
 import { getInitialAllLightState, getInitialState } from "../state/reducers/lights.reducer";
+import ModifySingleBulbModalComponent from "./modify-single-bulb.component";
 
 interface Props {}
 
 function Lights(props: Props) {
+  const [isModifyBulbModalVisible, setIsModifyBulbModalVisible] = useState(false);
   const {} = props;
   const dispatch = useAppDispatch();
   const lightsState = useAppSelector((state) => state.lights);
   const allLightsState = useAppSelector((state) => state.lights.allLights);
+
+
+  const handleOk = () => {
+    setIsModifyBulbModalVisible(false);
+;
+  };
+
+  const handleCancel = () => {
+    setIsModifyBulbModalVisible(false);
+;
+  };
+
 
   useEffect(() => {
     LightsService.getAllLights().then((val)=>{
@@ -31,19 +45,33 @@ function Lights(props: Props) {
     <>
       <div className="container lights-row">
         <div className="lights-row-item">
-          {lightsState.lights.map((light) => 
-            { return <Avatar style={{backgroundColor: `${allLightsState.on ? light.on? light.color: "grey" : "grey"}`}}
+          {lightsState.lights.map((light, index) => 
+            { return (
+              <>
+              <Button> Edit Bulb `${index +1}`</Button>
+                  <Avatar style={{backgroundColor: `${allLightsState.on ? light.on? light.color: "grey" : "grey"}`}}
                              className={allLightsState.on ? 
                                         light.on?
                                         allLightsState.pattern==1 ?
                                         'blinking lights-row-item' :
                                         allLightsState.pattern == 2 ?
                                         'blinking_2 lights-row-item'  :'lights-row-item' :'lights-row-item': 'lights-row-item'} 
-                             key={light.id}/>}           
+                             key={light.id}/>
+              </>
+            )
+        }           
           )}        
-        </div>
-   
+        </div>   
       </div>
+
+      <Modal title="Modify Bulb"
+             visible={isModifyBulbModalVisible}
+             footer={<Button onClick={handleCancel}>
+             Cancel
+            </Button>}
+            onCancel={handleCancel}>
+     <ModifySingleBulbModalComponent onChanged={handleOk} />
+     </Modal>
     </>
   );
 }
